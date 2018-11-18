91d626f0927352d89f206beb16dc54acaf809d73
sojurner <pykim0591@gmail.com>
2018-11-18 12:44:11 -0700

    Import grapql react apollo

--- client/components/SongList/index.js
+++ client/components/SongList/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
