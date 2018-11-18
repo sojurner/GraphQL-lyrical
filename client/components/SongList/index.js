574d5d002130e047f0a1043406470d7fa48a6f99
sojurner <pykim0591@gmail.com>
2018-11-18 12:44:26 -0700

    Conditional rendering of songs

--- client/components/SongList/index.js
+++ client/components/SongList/index.js
@@ -3,6 +3,21 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    if (!this.props.data.loading) {
+      console.log(this.props.data.songs);
+      return this.props.data.songs.map((song, index) => {
+        return (
+          <li key={song.id} className="collection-item">
+            {song.title}
+          </li>
+        );
+      });
+    } else {
+      return;
+    }
+  }
+
   render() {
     return <div>Lyrical</div>;
   }
